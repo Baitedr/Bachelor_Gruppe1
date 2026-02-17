eb5db844c0a8eb3df42581d477c769a581e3f4ee
Marcus Einarsen <marcuswhe@gmail.com>
2026-02-17 13:43:41 +0100

    Lagt mock sider for å kunne navigere

---
+++ frontend/src/components/JoinSession.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../CSScomponents.css';
+
+function JoinSession() {
+    return (
+        <div classanem="join-session-container">
+        <h1> Join Session</h1>
+        <p>Skriv inn kode for å bli med i presentajsonen</p>
+        </div>
+    );
+}
+
+export default JoinSession;
