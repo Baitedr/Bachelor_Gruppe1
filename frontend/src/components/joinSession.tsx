7461f3802a21cf406a7ea58b07bc118eb4f4ca28
martin <martin.mentzen.hansen@gmail.com>
2026-03-23 10:29:42 +0100

    fix import

--- frontend/src/components/joinSession.tsx
+++ frontend/src/components/joinSession.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import api from '../services/api';
-import '../../CSScomponents/PhoneInteraction.css';
+import '../CSScomponents/PhoneInteraction.css';
 import { Button } from './ui/button';
 import { Input } from './ui/input';
 import { Label } from './ui/label';
